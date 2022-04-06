414ab997470538752a03a42466edbf2c43bd3a39
aagraston <aagraston@gmail.com>
2022-04-06 13:22:13 -0700

    starting to set up the image selection page

--- src/pages/ImageSelectionPage.js
+++ src/pages/ImageSelectionPage.js
@@ -1,6 +1,27 @@
 import React from 'react'
+
+import image01 from '../imgs/img01.jpg'
+import image02 from '../imgs/img02.jpg'
+import image03 from '../imgs/img03.jpg'
+import image04 from '../imgs/img04.jpg'
+
 function ImageSelectionPage() {
-  return <div>Image Selection Page</div>
+  const imgArray = [image01, image02, image03, image04]
+
+  return (
+    <div>
+      <h1>Image Selector</h1>
+      <div className="image-selection-container">
+        {imgArray.map((img) => (
+          <div className="image-selection-card">
+            <div className="image-selection-image">
+              <img src={img} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default ImageSelectionPage
