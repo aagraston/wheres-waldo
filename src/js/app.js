e633e725c5ed9355093be295141f02934f2ef157
aagraston <aagraston@gmail.com>
2022-04-06 18:38:44 -0700

    refactor where image array is housed

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import image01 from '../imgs/img01.jpg'
+import image02 from '../imgs/img02.jpg'
+import image03 from '../imgs/img03.jpg'
+import image04 from '../imgs/img04.jpg'
+
 import ImagePage from '../pages/ImagePage'
 import ImageSelectionPage from '../pages/ImageSelectionPage'
 import ScoreBoardPage from '../pages/ScoreBoardPage'
 
 function App() {
+  const imgArray = [image01, image02, image03, image04]
+  const imgObjArray = []
+
+  imgArray.forEach((e, index) => {
+    const imageObj = {
+      image: e,
+      id: index,
+    }
+    imgObjArray.push(imageObj)
+  })
+
+  console.log(imgObjArray)
+
   const selectImage = (choice) => {
     console.log(choice)
   }
@@ -14,7 +32,12 @@ function App() {
     <Routes>
       <Route
         path="/"
-        element={<ImageSelectionPage selectImage={selectImage} />}
+        element={
+          <ImageSelectionPage
+            selectImage={selectImage}
+            imgObjArray={imgObjArray}
+          />
+        }
       />
       <Route path="image" element={<ImagePage />} />
       <Route path="score" element={<ScoreBoardPage />} />
--- src/pages/ImageSelectionPage.js
+++ src/pages/ImageSelectionPage.js
@@ -1,24 +1,7 @@
 import React from 'react'
 
-import image01 from '../imgs/img01.jpg'
-import image02 from '../imgs/img02.jpg'
-import image03 from '../imgs/img03.jpg'
-import image04 from '../imgs/img04.jpg'
-
 function ImageSelectionPage(props) {
-  const imgArray = [image01, image02, image03, image04]
-  const imgObjArray = []
-
-  imgArray.forEach((e, index) => {
-    const imageObj = {
-      image: e,
-      id: index,
-    }
-    imgObjArray.push(imageObj)
-  })
-
-  console.log(imgObjArray)
-  const { selectImage } = props
+  const { selectImage, imgObjArray } = props
 
   return (
     <div className="image-selection-page-container">
